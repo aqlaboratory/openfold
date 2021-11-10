78fa6c6ea9fd3724125fcf32ace3e9e8a0304f1d
Gustaf Ahdritz <gahdritz@gmail.com>
2021-11-09 23:59:08 -0500

    Use a more reasonable default for dataloader workers

--- openfold/config.py
+++ openfold/config.py
@@ -226,7 +226,7 @@ config = mlc.ConfigDict(
                 "use_small_bfd": False,
                 "data_loaders": {
                     "batch_size": 1,
-                    "num_workers": 1,
+                    "num_workers": 4,
                 },
             },
         },
