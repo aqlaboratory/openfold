cdaaceda5cc28609f05b1cb6243cd89248a14183
Gustaf Ahdritz <gahdritz@gmail.com>
2022-02-03 20:39:22 -0500

    Remove duplicate code

--- openfold/utils/loss.py
+++ openfold/utils/loss.py
@@ -1570,10 +1570,6 @@ class AlphaFoldLoss(nn.Module):
                 loss = loss.new_tensor(0., requires_grad=True)
             cum_loss = cum_loss + weight * loss
 
-        seq_len = torch.mean(batch["seq_length"].float())
-        crop_len = batch["aatype"].shape[-1]
-        cum_loss = cum_loss * torch.sqrt(min(seq_len, crop_len))
-
         # Scale the loss by the square root of the minimum of the crop size and
         # the (average) sequence length. See subsection 1.9.
         seq_len = torch.mean(batch["seq_length"].float())
