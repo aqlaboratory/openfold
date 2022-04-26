41c3b0bc4eb2b6f6c63aaae288f12faf6c539dbe
Gustaf Ahdritz <gahdritz@gmail.com>
2022-04-26 14:19:43 -0400

    Add licence notice to kernel files

--- openfold/utils/kernel/csrc/softmax_cuda_kernel.cu
+++ openfold/utils/kernel/csrc/softmax_cuda_kernel.cu
@@ -1,3 +1,19 @@
+// Copyright 2021 AlQuraishi Laboratory
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// modified from fastfold/model/fastnn/kernel/cuda_native/csrc/softmax_cuda_kernel.cu
+
 #include <math_constants.h>
 #include <torch/extension.h>
 #include <c10/cuda/CUDAGuard.h>
@@ -7,8 +23,6 @@
 #include "ATen/ATen.h"
 #include "ATen/cuda/CUDAContext.h"
 #include "compat.h"
-
-// modified from fastfold/model/fastnn/kernel/cuda_native/csrc/softmax_cuda_kernel.cu
 
 #define CHECK_CUDA(x) TORCH_CHECK(x.is_cuda(), #x " must be a CUDA tensor")
 #define CHECK_CONTIGUOUS(x) TORCH_CHECK(x.is_contiguous(), #x " must be contiguous")
