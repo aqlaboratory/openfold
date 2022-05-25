197029b1ed340d07c875b1ea4430fcfa10b98c33
Gustaf Ahdritz <gahdritz@gmail.com>
2022-05-25 16:00:52 -0400

    Fix config default

--- openfold/config.py
+++ openfold/config.py
@@ -271,7 +271,7 @@ config = mlc.ConfigDict(
                 "use_small_bfd": False,
                 "data_loaders": {
                     "batch_size": 1,
-                    "num_workers": 8,
+                    "num_workers": 16,
                 },
             },
         },
