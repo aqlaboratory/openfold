a35c44f52af2ef129def0f0294897b9be5bf46b6
Gustaf Ahdritz <gahdritz@gmail.com>
2022-06-23 23:17:05 -0400

    Fix formatting of install_hh_suite.sh

--- scripts/install_hh_suite.sh
+++ scripts/install_hh_suite.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git /tmp/hh-suite
-&& mkdir /tmp/hh-suite/build
-&& pushd /tmp/hh-suite/build
-&& cmake -DCMAKE_INSTALL_PREFIX=/opt/hhsuite ..
-&& make -j 4 && make install
-&& ln -s /opt/hhsuite/bin/* /usr/bin
-&& popd
-&& rm -rf /tmp/hh-suite
+git clone --branch v3.3.0 https://github.com/soedinglab/hh-suite.git /tmp/hh-suite \
+  && mkdir /tmp/hh-suite/build \
+  && pushd /tmp/hh-suite/build \
+  && cmake -DCMAKE_INSTALL_PREFIX=/opt/hhsuite .. \
+  && make -j 4 && make install \
+  && ln -s /opt/hhsuite/bin/* /usr/bin \
+  && popd \
+  && rm -rf /tmp/hh-suite
