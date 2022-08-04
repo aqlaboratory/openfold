ef239f308f48db2f12a2c5ab2d80b647a12d6e2c
Gustaf Ahdritz <gahdritz@gmail.com>
2022-08-04 17:56:37 -0400

    Remove print statement

--- setup.py
+++ setup.py
@@ -61,8 +61,6 @@ compute_capability, _ = get_nvidia_cc()
 if compute_capability is not None:
     compute_capabilities = set([compute_capability])
 
-print(compute_capabilities)
-
 cc_flag = []
 for major, minor in list(compute_capabilities):
     cc_flag.extend([
