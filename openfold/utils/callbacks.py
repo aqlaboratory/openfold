b9b8c0a5adfb92398307fbf99f5eb234ec78a66d
Gustaf Ahdritz <gahdritz@gmail.com>
2022-08-06 14:20:47 -0400

    Update early stopping callback

--- openfold/utils/callbacks.py
+++ openfold/utils/callbacks.py
@@ -6,8 +6,8 @@ class EarlyStoppingVerbose(EarlyStopping):
         The default EarlyStopping callback's verbose mode is too verbose.
         This class outputs a message only when it's getting ready to stop. 
     """
-    def _evalute_stopping_criteria(self, *args):
-        should_stop, reason = super()._evalute_stopping_criteria(*args)
+    def _evalute_stopping_criteria(self, *args, **kwargs):
+        should_stop, reason = super()._evalute_stopping_criteria(*args, **kwargs)
         if(should_stop):
             rank_zero_info(f"{reason}\n")
 
