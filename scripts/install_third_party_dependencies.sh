5e2c3058155c56706c9a8803fd51fceb1b9b3760
Tim O'Donnell <timodonnell@gmail.com>
2022-08-12 14:45:24 -0400

    Add --no-check-certificate to wget download (git.scicore.unibas.ch domain)
    Also drop the wget -q flag so errors are apparent

--- scripts/install_third_party_dependencies.sh
+++ scripts/install_third_party_dependencies.sh
@@ -27,7 +27,7 @@ pushd lib/conda/envs/$ENV_NAME/lib/python3.7/site-packages/ \
     && popd
 
 # Download folding resources
-wget -q -P openfold/resources \
+wget --no-check-certificate -P openfold/resources \
     https://git.scicore.unibas.ch/schwede/openstructure/-/raw/7102c63615b64735c4941278d92b554ec94415f8/modules/mol/alg/src/stereo_chemical_props.txt
 
 # Certain tests need access to this file
