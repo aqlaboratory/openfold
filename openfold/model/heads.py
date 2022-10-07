Fix precision bug

openfold/model/heads.py
@@ -150,15 +150,15 @@ class DistogramHead(nn.Module):
         logits = logits + logits.transpose(-2, -3)
         return logits
     
-    def forward(self, z):
-        
+    def forward(self, z): 
         float16_enabled = (torch.get_autocast_gpu_dtype() == torch.float16)
         if float16_enabled and torch.is_autocast_enabled():
             with torch.cuda.amp.autocast(enabled=False):
                 return self._forward(z.float())
         else:
             return self._forward(z)
-        
+
+
 class TMScoreHead(nn.Module):
     """
     For use in computation of TM-score, subsection 1.9.7

openfold/model/primitives.py
@@ -480,8 +480,9 @@ class Attention(nn.Module):
 
         # [*, Q, H, C_hidden]
         float16_enabled = (torch.get_autocast_gpu_dtype() == torch.float16)
-        if float16_enabled:
+        if float16_enabled and torch.is_autocast_enabled():
             use_memory_efficient_kernel = False
+        
         if(use_memory_efficient_kernel):
             if(len(biases) > 2):
                 raise ValueError(