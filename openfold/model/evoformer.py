bf1a58fec401c2a7605b2fa49415c9e53ce936fd
Sachin Kadyan <sachinkadyan7@gmail.com>
2022-10-18 17:06:06 -0400

    Added switching off of column attention in evoformer when using sequence embeddings. - Added flag `no_column_attention` in evoformer config. - Added check in `evoformer.py` to switch off `MSAColumnAttention` when the config flag `no_column_attention` is `True`.

--- openfold/model/evoformer.py
+++ openfold/model/evoformer.py
@@ -324,6 +324,7 @@ class EvoformerBlock(nn.Module):
         transition_n: int,
         msa_dropout: float,
         pair_dropout: float,
+        no_column_attention: bool,
         inf: float,
         eps: float,
     ):
@@ -337,12 +338,14 @@ class EvoformerBlock(nn.Module):
             inf=inf,
         )
 
-        self.msa_att_col = MSAColumnAttention(
-            c_m,
-            c_hidden_msa_att,
-            no_heads_msa,
-            inf=inf,
-        )
+        self.no_column_attention = no_column_attention
+        if self.no_column_attention == False:
+            self.msa_att_col = MSAColumnAttention(
+                c_m,
+                c_hidden_msa_att,
+                no_heads_msa,
+                inf=inf,
+            )
 
         self.msa_dropout_layer = DropoutRowwise(msa_dropout)
 
@@ -398,17 +401,18 @@ class EvoformerBlock(nn.Module):
             ),
             inplace=inplace_safe,
         )
-        
-        m = add(m, 
-            self.msa_att_col(
-                m, 
-                mask=msa_mask, 
-                chunk_size=chunk_size,
-                use_lma=use_lma,
-                use_flash=use_flash,
-            ),
-            inplace=inplace_safe,
-        )
+
+        if self.no_column_attention == False:
+            m = add(m,
+                self.msa_att_col(
+                    m,
+                    mask=msa_mask,
+                    chunk_size=chunk_size,
+                    use_lma=use_lma,
+                    use_flash=use_flash,
+                ),
+                inplace=inplace_safe,
+            )
 
         if(not inplace_safe):
             input_tensors = [m, input_tensors[1]]
@@ -596,6 +600,7 @@ class EvoformerStack(nn.Module):
         msa_dropout: float,
         pair_dropout: float,
         blocks_per_ckpt: int,
+        no_column_attention: bool,
         inf: float,
         eps: float,
         clear_cache_between_blocks: bool = False, 
@@ -659,6 +664,7 @@ class EvoformerStack(nn.Module):
                 transition_n=transition_n,
                 msa_dropout=msa_dropout,
                 pair_dropout=pair_dropout,
+                no_column_attention=no_column_attention,
                 inf=inf,
                 eps=eps,
             )
