57bf18284c08e1a2847a348c95d0e7969d628ac9
sachinkadyan7 <sachinkadyan7@gmail.com>
2023-09-11 22:45:15 -0700

    Properly reading the embedding file

--- openfold/data/data_pipeline.py
+++ openfold/data/data_pipeline.py
@@ -652,7 +652,7 @@ class DataPipeline:
             if (ext == ".pt"):
                 # Load embedding file
                 seqemb_data = torch.load(path)
-                seqemb_features["seq_embedding"] = seqemb_data
+                seqemb_features["seq_embedding"] = seqemb_data["representations"][33]
 
         return seqemb_features
 
