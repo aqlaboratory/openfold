eba00d85f7d182901af786eff4c29956a7ffbc4d
Jennifer <whereisthelux@gmail.com>
2024-03-13 01:53:37 -0400

    adds config recording to wandb

--- train_openfold.py
+++ train_openfold.py
@@ -401,6 +401,7 @@ def main(args):
             save_dir=args.output_dir,
             id=args.wandb_id,
             project=args.wandb_project,
+            config=config.to_dict(),
             **{"entity": args.wandb_entity}
         )
         loggers.append(wdb_logger)
