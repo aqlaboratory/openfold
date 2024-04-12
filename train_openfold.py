1952582615c3f3a3aaea705ffa6b5a408518d776
Jennifer Wei <jennifer.wei@omsf.io>
2024-04-12 10:14:57 +0000

    changes required for pytorch2

--- train_openfold.py
+++ train_openfold.py
@@ -401,7 +401,6 @@ def main(args):
             save_dir=args.output_dir,
             id=args.wandb_id,
             project=args.wandb_project,
-            config=config.to_dict(),
             **{"entity": args.wandb_entity}
         )
         loggers.append(wdb_logger)
