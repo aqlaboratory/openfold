0b5c9492ffe453e78ee1f2d1c9989d74e351d944
Lukas Jarosch <lukas.jarosch@gmx.net>
2024-05-05 23:49:22 -0700

    Give script more descriptive name

--- scripts/expand_alignment_duplicates.py
+++ scripts/expand_alignment_duplicates.py
@@ -1,9 +1,10 @@
 """
-The RODA database is non-redundant, meaning that it only stores one explicit
-representative alignment directory for all PDB chains in a 100% sequence
-identity cluster. In order to add explicit alignments for all PDB chains, this
-script will add the missing chain directories and symlink them to their
-representative alignment directories.
+The OpenProteinSet alignment database is non-redundant, meaning that it only
+stores one explicit representative alignment directory for all PDB chains in a
+100% sequence identity cluster. In order to add explicit alignments for all PDB
+chains, this script will add the missing chain directories and symlink them to
+their representative alignment directories. This is required in order to train
+OpenFold on the full PDB, not just one representative chain per cluster.
 """
 
 from argparse import ArgumentParser
@@ -51,6 +52,9 @@ def main(alignment_dir: Path, duplicate_chains_file: Path):
     # read duplicate chains file
     with open(duplicate_chains_file, "r") as fp:
         duplicate_chains = [list(line.strip().split()) for line in fp]
+
+    # convert to absolute path for symlink creation
+    alignment_dir = alignment_dir.resolve()
 
     create_duplicate_dirs(duplicate_chains, alignment_dir)
 
