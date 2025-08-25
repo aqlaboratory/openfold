4a430aaa41fbeed8742b6f9be95cfef3c9a12c22
Prashant Sohani <psohani@nvidia.com>
2025-08-25 10:54:59 -0700

    Add multiprocessing, relaxation_device options

---
+++ openfold/utils/multiprocessing/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: Copyright (c) 2025, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+from openfold.utils.multiprocessing.base_pipeline import BasePipeline, BaseTask
+from openfold.utils.multiprocessing.msa_task import MsaTask
+from openfold.utils.multiprocessing.feature_gen_task import FeatureGenTask
+from openfold.utils.multiprocessing.inference_task import InferenceTask
+from openfold.utils.multiprocessing.relaxation_task import RelaxationTask
